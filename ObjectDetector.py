a45c154d3a27573524590411987893d0ea9dcc83
CiudadAlex <ciudad.alex@gmail.com>
2024-08-16 12:07:35 +0200

    VideoCameraProcessor

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 class ObjectDetector:
 
     def __init__(self):
-        self.model = YOLO("yolov8n-cls.pt")  # load an official model
+        self.model = YOLO("yolov8n.pt")  # load an official model
 
     def predict(self, image_path):
         results = self.model(image_path)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ObjectDetector import ObjectDetector
-
+import math
 
 base_path = "C:/Alex/Dev/data_corpus/VideoCamera"
 
@@ -7,14 +7,32 @@ base_path = "C:/Alex/Dev/data_corpus/VideoCamera"
 def predict(obj_detector, image_name):
 
     print("#####################################")
-    result = obj_detector.predict(base_path + "/" + image_name)
-    print(result)
+    results = obj_detector.predict(base_path + "/" + image_name)
+
+    for r in results:
+
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+
+            # class name
+            cls = int(box.cls[0])
+
+            print(f"Class name: {r.names[cls]}. Confidence: {confidence}. Window ---> (x1, y1) = ({x1}, {y1}), (x2, y2) = ({x2}, {y2})")
 
 
 object_detector = ObjectDetector()
 
 predict(object_detector, "person1.jpg")
+
+'''
+predict(object_detector, "bus1.jpg")
 predict(object_detector, "car1.jpg")
 predict(object_detector, "dog1.jpg")
 predict(object_detector, "cat1.jpg")
-
+'''
