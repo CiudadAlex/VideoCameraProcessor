a13159064f15d0bfa16f6e003099ae276ecef4ae
CiudadAlex <ciudad.alex@gmail.com>
2024-08-27 23:06:03 +0200

    VideoCameraProcessor

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from detection.ObjectDetector import ObjectDetector
 from rtsp_client.RtspClient import RtspClient
 import time
+import os
 
 # base_path = "C:/Alex/Dev/data_corpus/VideoCamera"
 
+# FIXME https://docs.pyfilesystem.org/en/latest/reference/memoryfs.html
 
 def test_cam():
 
@@ -12,14 +14,19 @@ def test_cam():
     time.sleep(1)
     desired_class_name = "person"
 
-    for i in range(30):
+    for i in range(100):
 
         image_path = "./.out/" + str(i) + ".jpg"
         rtsp_client.save_screen(image_path)
+
         results = object_detector.predict(image_path)
 
         if ObjectDetector.is_there_object_class(results, desired_class_name):
             ObjectDetector.show_results(results)
+        else:
+            os.remove(image_path)
+
+        print(f"iteration = {i}")
 
     rtsp_client.close()
 
