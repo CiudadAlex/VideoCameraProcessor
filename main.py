46c4ebda9de94cf2034b6d6ac00250af92569b3f
CiudadAlex <ciudad.alex@gmail.com>
2024-08-27 22:58:36 +0200

    VideoCameraProcessor

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def test_cam():
     object_detector = ObjectDetector(size="m")
     rtsp_client = RtspClient.from_config_file('config.properties')
     time.sleep(1)
+    desired_class_name = "person"
 
-    for i in range(10):
+    for i in range(30):
 
         image_path = "./.out/" + str(i) + ".jpg"
         rtsp_client.save_screen(image_path)
         results = object_detector.predict(image_path)
 
-        ObjectDetector.show_results(results)
+        if ObjectDetector.is_there_object_class(results, desired_class_name):
+            ObjectDetector.show_results(results)
 
     rtsp_client.close()
 
