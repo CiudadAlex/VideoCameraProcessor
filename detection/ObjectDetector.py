8e0ea0c336ed99bd34fd538e1eb41b3a74ecedda
CiudadAlex <ciudad.alex@gmail.com>
2024-08-27 17:14:56 +0200

    VideoCameraProcessor

--- detection/ObjectDetector.py
+++ detection/ObjectDetector.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 class ObjectDetector:
 
     def __init__(self):
-        self.model = YOLO("yolov8x.pt")  # load an official model
+        self.model = YOLO("../yolov8x.pt")  # load an official model
 
     def predict(self, image_path):
         results = self.model.predict(source=image_path, conf=0.2)
