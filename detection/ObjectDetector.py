9f4b2a6c3910d3b60874569c109d9831e6c965e2
CiudadAlex <ciudad.alex@gmail.com>
2024-08-27 20:35:10 +0200

    VideoCameraProcessor

--- detection/ObjectDetector.py
+++ detection/ObjectDetector.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import math
 
 
 class ObjectDetector:
@@ -15,4 +16,39 @@ class ObjectDetector:
     def predict(self, image_path):
         results = self.model.predict(source=image_path, conf=0.7)
         return results
+
+    @staticmethod
+    def is_there_object_class(results, class_name):
+
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+
+                # class name
+                cls = int(box.cls[0])
+                class_name_box = r.names[cls]
+
+                if class_name_box == class_name:
+                    return True
+
+        return False
+
+    @staticmethod
+    def print_results(results):
+
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+                confidence = math.ceil((box.conf[0] * 100)) / 100
+
+                # class name
+                cls = int(box.cls[0])
+
+                print(f"Class name: {r.names[cls]}. Confidence: {confidence}. Window ---> (x1, y1) = ({x1}, {y1}), (x2, y2) = ({x2}, {y2})")
 
