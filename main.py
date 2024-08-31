aee7a9e302e676c5217706aa6722f93766089aa8
CiudadAlex <ciudad.alex@gmail.com>
2024-08-31 19:11:48 +0200

    VideoCameraProcessor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from processors.ShowDetectionsProcessor import ShowDetectionsProcessor
 from detection.ModelGenerator import ModelGenerator
 
 
-execute_action = "VideoCameraClassImageSaverProcessor"
+execute_action = "VideoCameraClassImageSaverProcessor_BIRDS"
 base_path = "C:/Alex/Dev/data_corpus/VideoCamera"
 
 
@@ -11,9 +11,13 @@ if execute_action == "ShowDetectionsProcessor":
     show_detections_processor = ShowDetectionsProcessor.load_with_custom_model(model_name="lightnings")
     show_detections_processor.show_detections(list_image_path=[base_path + "/lightning.jpg"])
 
-if execute_action == "VideoCameraClassImageSaverProcessor":
+if execute_action == "VideoCameraClassImageSaverProcessor_BIRDS":
     video_camera_class_image_saver_processor = VideoCameraClassImageSaverProcessor.load_with_custom_model(model_name="birds")
     video_camera_class_image_saver_processor.save_images_with_class(desired_class_name="bird", show_in_screen=True)
+
+if execute_action == "VideoCameraClassImageSaverProcessor_LIGHTNINGS":
+    video_camera_class_image_saver_processor = VideoCameraClassImageSaverProcessor.load_with_custom_model(model_name="lightnings")
+    video_camera_class_image_saver_processor.save_images_with_class(desired_class_name="Lightning", show_in_screen=True)
 
 if execute_action == "ModelGenerator":
     path_train = base_path + "/vidar.v2i.yolov8/data.yaml"
