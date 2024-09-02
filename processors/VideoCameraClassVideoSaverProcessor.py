69cc53e971ca78423a46a4c1d403bded8d4ce403
CiudadAlex <ciudad.alex@gmail.com>
2024-09-02 02:01:35 +0200

    VideoCameraProcessor

--- processors/VideoCameraClassVideoSaverProcessor.py
+++ processors/VideoCameraClassVideoSaverProcessor.py
@@ -3,7 +3,9 @@ from rtsp_client.RtspClient import RtspClient
 from utils.KeyboardInterrupter import KeyboardInterrupter
 from utils.ObjectDetectorContainer import ObjectDetectorContainer
 from utils.TimeRegulator import TimeRegulator
+from utils.CycleQueue import CycleQueue
 import time
+import threading
 
 
 class VideoCameraClassVideoSaverProcessor(ObjectDetectorContainer):
@@ -11,6 +13,7 @@ class VideoCameraClassVideoSaverProcessor(ObjectDetectorContainer):
     def __init__(self, object_detector):
         super().__init__(object_detector)
         self.rtsp_client = RtspClient.from_config_file('config.properties')
+        self.cycle_queue = CycleQueue(100)
 
         # To allow the client to connect correctly
         time.sleep(1)
@@ -25,21 +28,16 @@ class VideoCameraClassVideoSaverProcessor(ObjectDetectorContainer):
         keyboard_interrupter = KeyboardInterrupter(self.action_on_close)
         keyboard_interrupter.start()
 
+        thread_save_images_in_cycle_queue = threading.Thread(target=self.save_images_in_cycle_queue)
+        thread_save_images_in_cycle_queue.start()
+
+    def save_images_in_cycle_queue(self):
+
         time_regulator = TimeRegulator(interval_millis=40)
 
-        i = 0
-
-        start = time.time()
-
-        for i in range(100):
+        while True:
 
             pil_image = self.rtsp_client.get_pil_image()
+            self.cycle_queue.add(pil_image)
             time_regulator.wait_until_next_milestone()
 
-            i = i + 1
-            print(".")
-
-        end = time.time()
-        seconds = end - start
-        freq = 100 / seconds
-        print(">>>>> " + str(freq) + " per second")
