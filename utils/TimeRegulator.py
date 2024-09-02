e05faae48ed70c6b722eca63cb00a7f33f970f2a
CiudadAlex <ciudad.alex@gmail.com>
2024-09-02 10:51:14 +0200

    VideoCameraProcessor

--- utils/TimeRegulator.py
+++ utils/TimeRegulator.py
@@ -6,11 +6,14 @@ class TimeRegulator:
     def __init__(self, interval_millis):
         self.interval_millis = interval_millis
         self.start = time.time()
+        self.start_check_freq = time.time()
+        self.count_check_freq = 0
 
     def wait_until_next_milestone(self):
 
         now = time.time()
-        milliseconds_passed = now - self.start
+        seconds_passed = now - self.start
+        milliseconds_passed = 1000 * seconds_passed
 
         if milliseconds_passed >= self.interval_millis:
             # We are late. Do nothing
@@ -21,3 +24,19 @@ class TimeRegulator:
             time.sleep(millis_left/1000)
 
         self.start = time.time()
+        self.check_freq()
+
+    def check_freq(self):
+
+        self.count_check_freq = self.count_check_freq + 1
+        now = time.time()
+        seconds_passed = now - self.start_check_freq
+
+        if seconds_passed >= 1:
+            freq = self.count_check_freq / seconds_passed
+            print(">>>>> " + str(freq) + " per second")
+
+            # Reset
+            self.count_check_freq = 0
+            self.start_check_freq = time.time()
+
