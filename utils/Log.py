2810a839b53d98ebdbc85900ade624004ec22417
CiudadAlex <ciudad.alex@gmail.com>
2024-09-02 16:39:13 +0200

    VideoCameraProcessor

--- utils/Log.py
+++ utils/Log.py
@@ -1,11 +1,16 @@
 import logging
+import os
 
 
 class Log:
 
     @staticmethod
     def config():
-        logging.basicConfig(filename="./.out/log.txt",
+
+        path_log_file = "./.out/log.txt"
+        os.remove(path_log_file)
+
+        logging.basicConfig(filename=path_log_file,
                             filemode='a',
                             format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                             datefmt='%H:%M:%S',
