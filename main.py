d8c991cb3827bfb3dafff754297d05282df6b212
CiudadAlex <ciudad.alex@gmail.com>
2024-09-05 02:15:08 +0200

    VideoCameraProcessor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils.Log import Log
 from ActionExecutor import ActionExecutor
 
 
-if __name__ == '__main__':
+if __name__ == '__main__2':
     Log.config()
 
     '''
@@ -24,7 +24,7 @@ import ffmpeg
 
 packet_size = 4096
 
-process = ffmpeg.input('rtsp://deuterio:D3uT33RR10@192.168.0.21:554/stream1').output('./.out/audio.mp4', format='mulaw').run_async(pipe_stdout=True)
+process = ffmpeg.input('rtsp://xxxx:xxxx@192.168.0.21:554/stream1').output('./.out/audio.mp4', format='mulaw').run_async(pipe_stdout=True)
 packet = process.stdout.read(packet_size)
 
 while process.poll() is None:
@@ -32,21 +32,54 @@ while process.poll() is None:
     print(packet)
 '''
 
-'''
+import time
+from onvif import ONVIFCamera
+mycam = ONVIFCamera('192.168.0.21', 2020, 'deuterio', 'D3uT33RR10', './venv/Lib/site-packages/wsdl/')
 
-friendly_name: “Move left”
-command_on: ‘curl -k “http://xxx:xxx@192.168.1.41:8300/cgi-bin/hi3510/ptzctrl.cgi?-step=1&-act=left”’
 
-friendly_name: “Move right”
-command_on: ‘curl -k “http://xxx:xxx@192.168.1.41:8300/cgi-bin/hi3510/ptzctrl.cgi?-step=1&-act=right”’
+# Create ptz service
+ptz = mycam.create_ptz_service()
+print(ptz)
+media = mycam.create_media_service()
 
-friendly_name: “Move up”
-command_on: ‘curl -k “http://xxx:xxx@192.168.1.41:8300/cgi-bin/hi3510/ptzctrl.cgi?-step=1&-act=up”’
+# Get target profile
+media_profile = media.GetProfiles()[0]
 
-friendly_name: “Move down”
-command_on: ‘curl -k “http://xxx:xxx@192.168.1.41:8300/cgi-bin/hi3510/ptzctrl.cgi?-step=1&-act=down”’
+# Get PTZ configuration options for getting continuous move range
+request = ptz.create_type('GetConfigurationOptions')
+configuration_token = media_profile.PTZConfiguration.token
+request.ConfigurationToken = configuration_token
+ptz_configuration_options = ptz.GetConfigurationOptions(configuration_token)
 
-friendly_name: “Home”
-command_off: ‘curl -k “http://xxx:xxx@192.168.1.41:8300/cgi-bin/hi3510/ptzctrl.cgi?-step=1&-act=home
+moverequest = ptz.create_type('ContinuousMove')
+moverequest.ProfileToken = media_profile.token
 
-'''
+"""
+if moverequest.Velocity is None:
+    moverequest.Velocity = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
+    moverequest.Velocity.PanTilt.space = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].URI
+    moverequest.Velocity.Zoom.space = ptz_configuration_options.Spaces.ContinuousZoomVelocitySpace[0].URI
+"""
+
+# Get range of pan and tilt
+XMAX = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Max
+XMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Min
+YMAX = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Max
+YMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Min
+
+print(str(XMIN) + " " + str(XMAX) + " " + str(YMIN) + " " + str(YMAX))
+
+time.sleep(2)
+
+print ('move left...')
+#request.Velocity.PanTilt.x = XMIN
+#request.Velocity.PanTilt.y = 0
+
+def do_move(ptz, request):
+    # Start continuous move
+    # ptz.Stop({'ProfileToken': request.ProfileToken})
+    ptz.ContinuousMove(request)
+
+do_move(ptz, moverequest)
+
+
