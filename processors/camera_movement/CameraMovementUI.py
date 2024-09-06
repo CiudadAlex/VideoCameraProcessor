3b6e6ef5b9dbe46db84950c9f560dbf0d1a954ea
CiudadAlex <ciudad.alex@gmail.com>
2024-09-06 17:37:43 +0200

    VideoCameraProcessor

--- processors/camera_movement/CameraMovementUI.py
+++ processors/camera_movement/CameraMovementUI.py
@@ -3,20 +3,32 @@ import wx
 
 class CameraMovementUI(wx.Frame):
 
+    button_height = 50
+    button_width = 100
+
     def __init__(self):
         super().__init__(parent=None, title='Camera Control Pad')
         panel = wx.Panel(self)
 
-        CameraMovementUI.build_button_with_action(panel, 'UP', (55, 5), self.on_press_up)
-        CameraMovementUI.build_button_with_action(panel, 'DOWN', (55, 100), self.on_press_down)
-        CameraMovementUI.build_button_with_action(panel, 'LEFT', (5, 55), self.on_press_left)
-        CameraMovementUI.build_button_with_action(panel, 'RIGHT', (100, 55), self.on_press_right)
+        self.create_button_pad(panel)
 
         self.Show()
 
+    def create_button_pad(self, panel):
+
+        left_margin = 30
+        up_margin = 30
+        button_height = CameraMovementUI.button_height
+        button_width = CameraMovementUI.button_width
+
+        CameraMovementUI.build_button_with_action(panel, 'UP', (left_margin + button_width, up_margin), self.on_press_up)
+        CameraMovementUI.build_button_with_action(panel, 'DOWN', (left_margin + button_width, up_margin + 2 * button_height), self.on_press_down)
+        CameraMovementUI.build_button_with_action(panel, 'LEFT', (left_margin, up_margin + button_height), self.on_press_left)
+        CameraMovementUI.build_button_with_action(panel, 'RIGHT', (left_margin + 2 * button_width, up_margin + button_height), self.on_press_right)
+
     @staticmethod
     def build_button_with_action(panel, label, pos, action):
-        my_btn = wx.Button(panel, label=label, pos=pos)
+        my_btn = wx.Button(panel, label=label, pos=pos, size=(CameraMovementUI.button_width, CameraMovementUI.button_height))
         my_btn.Bind(wx.EVT_BUTTON, action)
 
     def on_press_up(self, event):
