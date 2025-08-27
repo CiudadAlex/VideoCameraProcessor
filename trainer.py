650512c602e42041c1b6145f9e6fa30bf978b955
CiudadAlex <ciudad.alex@gmail.com>
2025-08-28 01:48:15 +0200

    VideoCameraProcessor

---
+++ trainer.py
@@ -0,0 +1,17 @@
+from detection.ModelGenerator import ModelGenerator
+
+
+def exec_model_generator(train_dir, train_base_model, epochs):
+
+    base_path = "C:/Alex/Dev/data_corpus/VideoCamera"
+    path_train = base_path + "/" + train_dir + "/data.yaml"
+    return ModelGenerator.train_new_model(model_path=f"./.models/{train_base_model}.pt",
+                                          yaml_path=path_train,
+                                          epochs=epochs)
+
+
+train_dir = "cube.v3i.yolov8"
+train_base_model = "yolov8m.pt"
+epochs = 30
+results = exec_model_generator(train_dir, train_base_model, epochs)
+print(results)
